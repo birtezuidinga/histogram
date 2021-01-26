81cd30e8b099a47449e6e95770978eb9e245abbc
Nik Drummond <nikolasdrummond@gmail.com>
2021-01-26 15:32:07 +0100

    data and graph addition

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #This is main script
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+rand = np.random.randint(1,100,1000)
+
+fig, ax = plt.subplot(figsize = (10,5))
+ax = plt.hist(x=rand, bins='auto', color='#0504aa',
+                            alpha=0.7, rwidth=0.85)
