5e0a7052558704d0d2dff6ada0d027204637dce3
birtezuidinga <zuidinga@neuro.mpg.de>
2021-01-26 15:39:44 +0100

    Plotting works

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 rand = np.random.randint(1,100,1000)
 
-fig, ax = plt.subplot(figsize = (10,5))
 ax = plt.hist(x=rand, bins='auto', color='#0504aa',
                             alpha=0.7, rwidth=0.85)
+
+plt.show()
